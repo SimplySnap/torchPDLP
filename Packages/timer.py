785b703655c6d2a24a9abb2865e837e9f80c462e
connorphillips700 <connorphillips700@gmail.com>
2025-07-03 15:19:52 -0700

    Create timer.py

---
+++ Packages/timer.py
@@ -0,0 +1,25 @@
+from time import perf_counter
+
+class Timer:
+  """
+  Timer class to measure execution time of code blocks.
+  Usage:
+  
+    with Timer("Label"):
+        # Code block to be timed
+
+  Output:
+    Label: <time in seconds> seconds
+  """
+    # ChatGPT wrote this and I don't know how it works
+  def __init__(self, label="Elapsed time"):
+      self.label = label
+
+  def __enter__(self):
+      self.start = perf_counter()
+      return self
+
+  def __exit__(self, *args):
+      self.end = perf_counter()
+      self.elapsed = self.end - self.start
+      print(f"{self.label}: {self.elapsed:.6f} seconds")
