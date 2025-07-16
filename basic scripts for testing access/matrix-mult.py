5840f8e85a5a1d24146795888c52ac65cb27a656
Titus Parker <titus000@stanford.edu>
2025-07-16 10:48:57 -0700

    Matrix mult changed to be more realistic test of cluster

--- basic scripts for testing access/matrix-mult.py
+++ basic scripts for testing access/matrix-mult.py
@@ -1,11 +1,17 @@
 import numpy as np
-import torch
+import time
+from timeit import default_timer as timer
 
 #Make random 10,000*10,000 matrix
+start = timer() #Start of timer
 n = 10000
 m = 10000
 
-mat1 = np.random.random_integers(-5,5,(n,m))
-mat2 = np.random.random_integers(-5,5,(n,m))
+mat1 = np.random.randint(-5,5,(n,m))
+mat2 = np.random.randint(-5,5,(n,m))
 
 mat = mat1@mat2
+end = timer() #end of timer
+
+print( mat[:5], (end-start))
+
