2e6b2d58084624dd84527273fa1b3a4f9cbb1b2c
connorphillips700 <connorphillips700@gmail.com>
2025-07-30 18:47:38 -0700

    Update pdhg_solver.py

--- Packages/pdhg_solver.py
+++ Packages/pdhg_solver.py
@@ -50,3 +50,56 @@ def pdhg_solver(mps_file_path, max_iter=10000, tol=1e-4, term_period=1000, verbo
     print(minimizer[:10].cpu().numpy())
 
     return minimizer, obj_val, iterations
+
+def restarted_solver(mps_file_path, tol=1e-4, restart_period=40, verbose=True):
+    """
+    Full Restarted PDHG solver implementation using PyTorch.
+
+    Args:
+      mps_file_path (str): Path to the MPS file.
+      tol (float, optional): Tolerance for convergence. Defaults to 1e-4. Use 1e-8 for high accuracy
+      term_period (int, optional): Period for termination checks. Defaults to 1000.
+      verbose (bool, optional): Whether to print termination check information. Defaults to True.
+
+    Returns:
+      The minimizer, objective value, and number of iterations and restarts for convergence.
+    """
+    import torch
+    from mps_to_standard_form import mps_to_standard_form_torch
+    from restarted_pdhg_algorithm import restarted_pdhg
+
+    # --- Device Selection ---
+    if torch.cuda.is_available():
+        device = torch.device('cuda')
+        print(f"PyTorch is using ROCm/CUDA device: {torch.cuda.get_device_name(0)}") if verbose else None
+    else:
+        device = torch.device('cpu')
+        print("ROCm/CUDA not available. PyTorch is using CPU.") if verbose else None
+
+    # --- Parameter Loading ---
+    try:
+          c, G, h, A, b, l, u = mps_to_standard_form_torch("/content/PDLP-AMD-RIPS/datasets/well_conditioned.mps", device=device)
+    except Exception as e:
+        print(f"Failed to load MPS file: {e}")
+        exit(1)
+
+    is_neg_inf = torch.isinf(l) & (l < 0)
+    is_pos_inf = torch.isinf(u) & (u > 0)
+
+    l_dual = l.clone()
+    u_dual = u.clone()
+
+    l_dual[is_neg_inf] = 0
+    u_dual[is_pos_inf] = 0
+
+    # --- Run PDHG Solver on the GPU or CPU ---
+    minimizer, objective_value, total_iterations, total_restarts = restarted_pdhg(c, G, h, A, b, l, u, is_neg_inf, is_pos_inf, l_dual, u_dual, device, tol=tol, verbose=verbose, restart_period=restart_period)
+
+    if verbose:
+      print("Objective Value:", objective_value)
+      print("Iterations:", total_iterations)
+      print("Restarts:", total_restarts)
+      print("\nMinimizer (first 10 variables):")
+      print(minimizer[:10].cpu().numpy())
+
+    return minimizer, objective_value, total_iterations, total_restarts
