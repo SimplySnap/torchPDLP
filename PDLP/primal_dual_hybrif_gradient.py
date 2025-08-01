153f4e4abeb40f084978f2fdd581065de10ac0b0
胡夕颜 <huxiyan@Sias-MacBook.local>
2025-07-31 21:56:20 -0700

    not working

--- PDLP/primal_dual_hybrif_gradient.py
+++ PDLP/primal_dual_hybrif_gradient.py
@@ -37,92 +37,94 @@ def pdlp_algorithm(K, m_ineq, c, q, l, u, device, max_iter=100_000, tol=1e-4, ve
     
     # -------------- Outer Loop --------------
     while k < max_iter:
-      t = 0 # Initialize inner iteration counter
-
-      # Initialize/Reset sums for averaging
-      x_eta_total = torch.zeros_like(x)
-      y_eta_total = torch.zeros_like(y)
-      eta_total = 0
-
-      # Initialize/Reset Previous restart point for primal weighting
-      x_last_restart = x.clone()
-      y_last_restart = y.clone()
-      # --------- Inner Loop ---------
-      while k < max_iter:
-        x_previous = x.clone() # For checking necessary criteria
-        y_previous = y.clone()
-
-        # Regular or Adaptive step of pdhg
-        x, y, eta, eta_hat, j = adaptive_one_step_pdhg(x, y, c, q, K, l, u, m_ineq, eta, omega, theta, k, j)
-        # Increase iteration counters
-        k += 1
-        t += 1
-
-        # Update totals
-        x_eta_total += eta * x
-        y_eta_total += eta * y
-        eta_total += eta
+        t = 0 # Initialize inner iteration counter
         
-        # Update eta
-        eta = eta_hat
+        # Initialize/Reset sums for averaging
+        x_eta_total = torch.zeros_like(x)
+        y_eta_total = torch.zeros_like(y)
+        eta_total = 0
+        
+        # Initialize/Reset Previous restart point for primal weighting
+        x_last_restart = x.clone()
+        y_last_restart = y.clone()
+        
+        # --------- Inner Loop ---------
+        while k < max_iter:
+            
+            k += 1
+            x_previous = x.clone() # For checking necessary criteria
+            y_previous = y.clone()
 
-        # Check Restart Criteria Every restart_period iterations
-        if t % restart_period == 0:
-          # Compute averages
-          x_avg = x_eta_total / eta_total
-          y_avg = y_eta_total / eta_total
+            # Adaptive step of pdhg
+            x, y, eta, eta_hat, j = adaptive_one_step_pdhg(x, y, c, q, K, l, u, m_ineq, eta, omega, theta, k, j)
+            
+            # Increase iteration counters
+            t += 1
 
-          # Compute KKT errors
-          KKT_current = KKT_error(x, y, c, q, K, m_ineq, omega, is_neg_inf, is_pos_inf, l_dual, u_dual, device)
-          KKT_average = KKT_error(x_avg, y_avg, c, q, K, m_ineq, omega, is_neg_inf, is_pos_inf, l_dual, u_dual, device)
+            # Update totals
+            x_eta_total += eta * x
+            y_eta_total += eta * y
+            eta_total += eta
+        
+            # Update eta
+            eta = eta_hat
 
-          KKT_min = min(KKT_current, KKT_average)
+            # Check Restart Criteria Every restart_period iterations
+            if t % restart_period == 0:
+                
+                # Compute averages
+                x_avg = x_eta_total / eta_total
+                y_avg = y_eta_total / eta_total
 
-          KKT_previous = KKT_error(x_previous, y_previous, c, q, K, m_ineq, omega, is_neg_inf, is_pos_inf, l_dual, u_dual, device) # For checking necessary criteria
+                # Compute KKT errors
+                KKT_current = KKT_error(x, y, c, q, K, m_ineq, omega, is_neg_inf, is_pos_inf, l_dual, u_dual, device)
+                KKT_average = KKT_error(x_avg, y_avg, c, q, K, m_ineq, omega, is_neg_inf, is_pos_inf, l_dual, u_dual, device)
+                KKT_min = min(KKT_current, KKT_average)
+                KKT_previous = KKT_error(x_previous, y_previous, c, q, K, m_ineq, omega, is_neg_inf, is_pos_inf, l_dual, u_dual, device) # For checking necessary criteria
 
-          # Add three kkt passes
-          j += 3
+                # Add three kkt passes
+                j += 3
+            
+                # Check Restart Criteria and update with Restart Candidate
+                if KKT_min <= beta[0] * KKT_first: # Sufficient Criteria
+                    print(f"Sufficient restart at iteration {t} using the", "Average iterate." if KKT_current >= KKT_average else "Current iterate.") if verbose else None
+                    (x, y) = (x_avg, y_avg) if KKT_current >= KKT_average else (x, y)
+                    break
+                elif KKT_min <= beta[1] * KKT_first and KKT_min > KKT_previous: # Necessary Criteria
+                    print(f"Necessary restart at iteration {t} using the", "Average iterate." if KKT_current >= KKT_average else "Current iterate.") if verbose else None
+                    (x, y) = (x_avg, y_avg) if KKT_current >= KKT_average else (x, y)
+                    break
+                elif t >= beta[2] * k: # Artificial Criteria
+                    print(f"Artificial restart at iteration {t} using the", "Average iterate." if KKT_current >= KKT_average else "Current iterate.") if verbose else None
+                    (x, y) = (x_avg, y_avg) if KKT_current >= KKT_average else (x, y)
+                    break
+        # ------------- End Inner Loop ------------
+        n += 1 # Increase restart loop counter
 
-          # Check Restart Criteria and update with Restart Candidate
-          if KKT_min <= beta[0] * KKT_first: # Sufficient Criteria
-            print(f"Sufficient restart at iteration {t} using the", "Average iterate." if KKT_current >= KKT_average else "Current iterate.") if verbose else None
-            (x, y) = (x_avg, y_avg) if KKT_current >= KKT_average else (x, y)
-            break
-          elif KKT_min <= beta[1] * KKT_first and KKT_min > KKT_previous: # Necessary Criteria
-            print(f"Necessary restart at iteration {t} using the", "Average iterate." if KKT_current >= KKT_average else "Current iterate.") if verbose else None
-            (x, y) = (x_avg, y_avg) if KKT_current >= KKT_average else (x, y)
-            break
-          elif t >= beta[2] * k: # Artificial Criteria
-            print(f"Artificial restart at iteration {t} using the", "Average iterate." if KKT_current >= KKT_average else "Current iterate.") if verbose else None
-            (x, y) = (x_avg, y_avg) if KKT_current >= KKT_average else (x, y)
-            break
-      # ------------- End Inner Loop ------------
-      n += 1 # Increase restart loop counter
+        if primal_update: # Primal weight update
+            delta_x = torch.norm(x - x_last_restart, p=2)
+            delta_y = torch.norm(y - y_last_restart, p=2)
+            omega = torch.sqrt(omega * (delta_y / delta_x)) if delta_x > 1e-6 and delta_y > 1e-6 else omega
 
-      if primal_update: # Primal weight update
-        delta_x = torch.norm(x - x_last_restart, p=2)
-        delta_y = torch.norm(y - y_last_restart, p=2)
-        omega = torch.sqrt(omega * (delta_y / delta_x)) if delta_x > 1e-6 and delta_y > 1e-6 else omega
-
-      KKT_first = KKT_error(x, y, omega) # Update KKT_first for next iteration (incase primal weight updates)
-      j += 1 # Add one kkt pass
+        KKT_first = KKT_error(x, y, c, q, K, m_ineq, omega, is_neg_inf, is_pos_inf, l_dual, u_dual, device) # Update KKT_first for next iteration (incase primal weight updates)
+        j += 1 # Add one kkt pass
       
-      # Compute primal and dual residuals, and duality gap
-      primal_residual, dual_residual, duality_gap, prim_obj, adjusted_dual = compute_residuals_and_duality_gap(x, y, c, q, K, m_ineq, is_neg_inf, is_pos_inf, l_dual, u_dual)
+        # Compute primal and dual residuals, and duality gap
+        primal_residual, dual_residual, duality_gap, prim_obj, adjusted_dual = compute_residuals_and_duality_gap(x, y, c, q, K, m_ineq, is_neg_inf, is_pos_inf, l_dual, u_dual)
 
-      # Add one kkt pass
-      j += 1
+        # Add one kkt pass
+        j += 1
 
-      if verbose:
-          print(f"[{k}] Primal Obj: {prim_obj.item():.4f}, Adjusted Dual Obj: {adjusted_dual.item():.4f}, "
-                f"Gap: {duality_gap.item()/ (1 + abs(prim_obj.item()) + abs(adjusted_dual.item())):.2e}, Prim Res: {primal_residual.item() / (1 + q_norm):.2e}, Dual Res: {dual_residual.item() / (1 + c_norm):.2e}")
-          print("")
+        if verbose:
+            print(f"[{k}] Primal Obj: {prim_obj.item():.4f}, Adjusted Dual Obj: {adjusted_dual.item():.4f}, "
+                    f"Gap: {duality_gap.item()/ (1 + abs(prim_obj.item()) + abs(adjusted_dual.item())):.2e}, Prim Res: {primal_residual.item() / (1 + q_norm):.2e}, Dual Res: {dual_residual.item() / (1 + c_norm):.2e}")
+            print("")
 
-      # Termination conditions
-      if check_termination(primal_residual, dual_residual, duality_gap, prim_obj, adjusted_dual, q_norm, c_norm, tol):
-          if verbose:
+        # Termination conditions
+        if check_termination(primal_residual, dual_residual, duality_gap, prim_obj, adjusted_dual, q_norm, c_norm, tol):
+            if verbose:
               print(f"Converged at iteration {k} restart loop {n}")
-          break
+            break
     # ------------------- End Outer Loop ------------------------
-
+    
     return x, prim_obj.cpu().item(), k, n, j
