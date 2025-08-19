26d3c59b327ce5f039f8a857ed1307126869b186
connorphillips700 <connorphillips700@gmail.com>
2025-08-19 00:04:48 -0700

    Update main.py

--- torchPDLP/torchPDLP/main.py
+++ torchPDLP/torchPDLP/main.py
@@ -40,7 +40,7 @@ def LPSolver(mps_file_path, tol=1e-4, device='auto', precondition=True, primal_w
 
     if verbose:
       print(f"\nConfiguration:")
-      print(f"Instance path: {mps_folder_path}")
+      print(f"Instance path: {mps_file_path}")
       print(f"Tolerance: {tol}")
       print(f"Output path: {output_path}")
       print(f"Time limit: {time_limit} seconds")
